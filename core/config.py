5a5bf16caa6598eb2fc6704ae15b55e8fb9da9e7
ODD SOUL <vijivenkadesh@gmail.com>
2026-03-21 17:03:49 +0530

    Embeddings added

--- core/config.py
+++ core/config.py
@@ -1,13 +1,21 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field, field_validator
+from pydantic import SecretStr
+from typing import Optional
+import os
 
 
 
 
 class EnvSettings(BaseSettings):
-    OPENAI_API_KEY: str = Field(description="This is API key for Open AI",
-                                default="",
+    OPENAI_API_KEY: Optional[SecretStr] = Field(description="This is API key for Open AI",
+                                default=None,
                                 strict=True)
+    LANGSMITH_TRACING: str = "true"
+    LANGSMITH_ENDPOINT: str = ""
+    LANGSMITH_API_KEY: str = ""
+    LANGSMITH_PROJECT: str = ""
+    MODEL: str = ""
     
 
     model_config = SettingsConfigDict(
@@ -23,9 +31,16 @@ class EnvSettings(BaseSettings):
         if not v.startswith(prefix):
             raise ValueError(f"API key must start with '{prefix}'")
         return v
+    
+    def load_langsmith(self):
+        os.environ['LANGSMITH_TRACING'] = self.LANGSMITH_TRACING
+        os.environ['LANGSMITH_ENDPOINT'] = self.LANGSMITH_ENDPOINT
+        os.environ['LANGSMITH_API_KEY'] = self.LANGSMITH_API_KEY
+        os.environ['LANGSMITH_PROJECT'] = self.LANGSMITH_PROJECT
 
 
 try:
     settings = EnvSettings()
+    settings.load_langsmith()
 except ValueError as e:
     print(f"This is the error {str(e)}")
--- utils/embedding-pipeline.py
+++ utils/embedding-pipeline.py
@@ -1,8 +1,17 @@
+import sys
+import os
 import logging
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from documentloader import PdfLoader
-import sys
+from langchain_openai import OpenAIEmbeddings
+from pydantic import SecretStr
+from dotenv import load_dotenv
+from typing import List, Any
+from langchain_core.documents import Document
 
+load_dotenv()
+
+# Setting up logging
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 formatter = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
@@ -10,16 +19,41 @@ console_handler = logging.StreamHandler(stream=sys.stdout)
 console_handler.setFormatter(fmt=formatter)
 logger.addHandler(hdlr=console_handler)
 
-loader = PdfLoader()
-documents  = loader.load_pdf()
 
-# print(documents)
 
-splitter = RecursiveCharacterTextSplitter()
 
-chunks = splitter.split_documents(documents=documents)
+class EmbeddingManager:
+    OPENAI_API_KEY = SecretStr(f"{os.getenv("OPENAI_API_KEY")}")
+    # Load the PDF documents and split them into chunks
+    @classmethod
+    def get_chunks(cls, filepath: str):
+        try:
+            loader = PdfLoader(filepath=filepath)
+        except FileNotFoundError as e:
+            logger.error(msg=f"{e}")
+        documents  = loader.load_pdf()
 
-logger.info(chunks[0].metadata)
+        if not documents:
+            logger.error(msg="No documents found")
+            raise IndexError("No documents found")
+        
+        splitter = RecursiveCharacterTextSplitter()
+        chunks = splitter.split_documents(documents=documents)
+        logger.info(msg=f"{len(chunks)} chunks are created")
+        return chunks
 
+    @classmethod
+    def get_embeddings(cls) -> OpenAIEmbeddings:
+        embeddings  = OpenAIEmbeddings(api_key=cls.OPENAI_API_KEY)
+        # response = embeddings.embed_documents(texts=chunks)
+        return embeddings
+
+    # print(response)
 
 # TODO: add metadata to the chunks
+if __name__ == "__main__":
+    try:
+        test_chunks = EmbeddingManager.get_chunks(filepath="doc") 
+        print(test_chunks[0])
+    except Exception as e:
+        logger.error(e)
